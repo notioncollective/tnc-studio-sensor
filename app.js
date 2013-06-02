5837b682a1b424494c64ad94baa7c42eb1a45370
Andy Dayton <amdayton@gmail.com>
2013-06-02 12:44:51 -0400

    Added interval to temp polling

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var exec = require('child_process').exec,
-	child,
+	// cmd = 'echo Test',
+	cmd = './lib/Adafruit_DHT_Driver/Adafruit_DHT 11 4',
+	interval = setInterval(getTemp, 3000);
 
-	cmd = './lib/Adafruit_DHT_Driver/Adafruit_DHT 11 4'
+function getTemp() {
+	var child = exec(cmd, function(error, stdout, stderr) {
+		console.log('stdout: '+stdout);
+		console.log('stderr: '+stderr);
 
-child = exec(cmd, function(error, stdout, stderr) {
-	console.log('stdout: '+stdout);
-	console.log('stderr: '+stderr);
+		if(error !== null) {
+			console.log('exec error: '+error);
+		}
+	});
 
-	if(error !== null) {
-		console.log('exec error: '+error);
-	}
-});
+	return child;
+}
+
