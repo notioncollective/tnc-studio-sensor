e384bb6e64d6ab8ac6138e9d4c9eae3832c96899
Andy Dayton <amdayton@gmail.com>
2013-06-02 13:39:39 -0400

    Only output data when recieved from sensor

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var fs = require('fs'),
 	exec = require('child_process').exec,
+	child;
 
 	config = JSON.parse(fs.readFileSync('config.json', encoding='ascii')),
 	// cmd = 'echo Test',
 	cmd = config.script+' '+config.device+' '+config.pin,
 	interval = setInterval(getTemp, 3000);
 
-console.log(cmd);
 
+/**
+ * Runs Adafruit script to grab the temp
+ */
 function getTemp() {
-	var child = exec(cmd, function(err, stdout, stderr) {
+	child = exec(cmd, function(err, stdout, stderr) {
 		var lines,
 				matches,
 				temp,
@@ -18,21 +21,24 @@ function getTemp() {
 		
 		if(!err) {
 			lines = stdout.split('\n');
+			// make sure there's a line with the temp/humidity data
+			// sometimes we don't get data back
 			if(lines[2]) {
 				matches = /Temp = (\d+) \*C\, Hum = (\d+) \%/.exec(lines[2]);
 				if(matches) {
 					temp = matches[1];
 					hum = matches[2]
 				}
+				
+				// data goes in an object
+				data = {
+					temp: temp,
+					hum: hum
+				}
+				
+				console.log(data);
+		
 			}
-			
-			// data goes in an object
-			data = {
-				temp: temp,
-				hum: hum
-			}
-			
-			console.log(data);
 			
 		} else {
 			console.log("There was an error!\n"+stderr);
